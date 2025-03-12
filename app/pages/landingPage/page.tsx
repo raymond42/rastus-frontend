2e1324ac7ae31f24bc8ed570f53b8f0931b8ecf6
Raymond <raymond@zazuafrica.com>
2025-03-13 00:37:44 +0200

    feat: add landing page

--- app/pages/landingPage/page.tsx
+++ app/pages/landingPage/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import HeroBackground from "@/public/hero-bg.png";
 import Navbar from "@/app/components/Navbar";
+import { frankRuhlLibrev, frankRuhlLibrevBold } from "@/app/utils/fonts";
 
 const LandingPage = () => {
   return (
@@ -10,6 +11,22 @@ const LandingPage = () => {
         <Image src={HeroBackground} alt="hero background" />
       </div>
       <Navbar />
+      <div className="px-40 flex flex-col justify-center h-screen gap-16 w-1/2">
+        <h1
+          className={`${frankRuhlLibrevBold.className} text-[78px] font-bold text-primary`}
+        >
+          Find the Best Fashion Style For You
+        </h1>
+        <p className={`${frankRuhlLibrev.className} text-[22px] text-primary`}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+          ullamcorper congue eros, eget tincidunt ipsum eleifend ut.{" "}
+        </p>
+        <button
+          className={`${frankRuhlLibrev.className} text-[18px] border border-primary text-white bg-primary hover:bg-transparent hover:text-primary w-[223px] h-[64px] rounded transition-colors`}
+        >
+          SHOP NOW
+        </button>
+      </div>
     </div>
   );
 };
