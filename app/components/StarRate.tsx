13d3c82818111ac3ede8f92387074252586997d4
Raymond <raymond42.gr@gmail.com>
2025-04-02 14:43:01 +0200

    fix the build

--- app/components/StarRate.tsx
+++ app/components/StarRate.tsx
@@ -5,19 +5,15 @@ import { FaStar } from "react-icons/fa";
 const StarRate = () => {
   const [rating, setRating] = useState<number | null>(null);
 
-  const [rateColor, setRateColor] = useState<string | null>(null);
-
   return (
     <div className="flex gap-1 cursor-pointer">
-      {[...Array(5)].map((star, key) => {
+      {[...Array(5)].map((_, key) => {
         const currentRate = key + 1;
         return (
           <FaStar
             key={key}
             size={20}
-            color={
-              (rating && currentRate <= rating) || rateColor ? "orange" : "grey"
-            }
+            color={rating && currentRate <= rating ? "orange" : "grey"}
             aria-valuenow={currentRate}
             onClick={() => setRating(currentRate)}
           />
