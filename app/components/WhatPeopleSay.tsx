6ad98f78128fe1e929243557e2c93fd80a04bade
Raymond Gakwaya <45050345+raymond42@users.noreply.github.com>
2025-04-05 17:59:36 +0200

    release to main (#30)
    remove unnecessary comments

--- app/components/WhatPeopleSay.tsx
+++ app/components/WhatPeopleSay.tsx
@@ -1,39 +1,10 @@
-// import React from "react";
-// import { frankRuhlLibrev, frankRuhlLibrevBold } from "../utils/fonts";
-// import CardSlider from "./CardSlider";
-
-// const WhatPeopleSay = () => {
-//   return (
-//     <div className="max-h-screen">
-//       <div className="w-full flex gap-5 flex-col justify-center items-center p-10">
-//         <h1
-//           className={`${frankRuhlLibrevBold.className} text-[48px] text-primary`}
-//         >
-//           What People Say About Us
-//         </h1>
-//         <p
-//           className={`${frankRuhlLibrev.className} text-[20px] leading-[180%]`}
-//         >
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-//           ullamcorper congue eros
-//         </p>
-//       </div>
-//       <div className="px-4 sm:px-0">
-//         <CardSlider />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default WhatPeopleSay;
-
 import React from "react";
 import { frankRuhlLibrev, frankRuhlLibrevBold } from "../utils/fonts";
 import CardSlider from "./CardSlider";
 
 const WhatPeopleSay = () => {
   return (
-    <div className="man-h-screen w-full flex flex-col items-center">
+    <div className="min-h-screen w-full flex flex-col items-center">
       <div className="w-full flex flex-col gap-5 items-center px-4 sm:px-8 lg:px-16 text-center py-10">
         <h1
           className={`${frankRuhlLibrevBold.className} text-[32px] sm:text-[40px] lg:text-[48px] text-primary`}
