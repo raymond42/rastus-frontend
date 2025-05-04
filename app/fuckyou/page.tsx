f24d10aa94f490baf2905db79d217d1dd7d9bdaa
Raymond <raymond42.gr@gmail.com>
2025-05-04 02:25:22 +0200

    fix f page

--- app/fuckyou/page.tsx
+++ app/fuckyou/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const FuckYou = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-cover bg-center text-white">
-      <div className="bg-black bg-opacity-70 p-10 md:p-20 rounded-3xl text-center shadow-2xl backdrop-blur-sm max-w-2xl">
+      <div className="bg-black bg-opacity-70 p-5 lg:p-20 rounded-3xl text-center shadow-2xl backdrop-blur-sm max-w-2xl">
         <h1 className="text-6xl md:text-7xl font-extrabold mb-6 tracking-wide">
           Fuck You 🖕🏽
         </h1>
